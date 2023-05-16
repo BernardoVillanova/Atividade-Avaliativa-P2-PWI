5e2a554805e61a3d3fc2d9cccd89cadb4ba29037
bern <bern@debian-BULLSEYE-live-builder-AMD64>
2023-05-16 12:39:03 -0300

    cart is working

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let slideIndex = 1;
+var n1;
+var res = 0;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -14,15 +16,20 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 } 
+
+function addCar(n1) {
+    let total = document.getElementById('val') 
+
+    res = res + n1;
+
+    total.innerHTML = `${res.toFixed(2)}`
+    
+
+}
